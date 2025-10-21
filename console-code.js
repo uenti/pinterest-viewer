b46ee63063e40b1b10ac054ccf71fd15ab48b18b
never again <2hu@tuta.io>
2025-10-21 10:25:29 +0000

    added auto downloading
    no longer need to go through the image opening in a new tab <3

--- console-code.js
+++ console-code.js
@@ -1,16 +1,24 @@
 (function() {
     const img = document.querySelector("img");
+    if (!img) return;
 
     const imageURL = img.src;
+    const fileName = imageURL.split("/").pop().split("?")[0]
     
-    const fileName = imageURL.split("/").pop().split("?")[0];
-    
-    const a = document.createElement("a")
-    
-    a.href = imageURL
-    a.download = fileName
-    document.body.appendChild(a);
-    a.click();
-}
+    fetch(imageURL, {mode: "cors"})
+        .then(respone => {
+            return respone.blob();
+    })
 
-)();
+    .then(blob => {
+        const blobUrl = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = blobUrl;
+        a.download = fileName;
+        document.body.appendChild(a);
+        a.click();
+        URL.revokeObjectURL(blobUrl);
+    })
+
+})();
+
